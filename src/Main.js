85b33fe62fa8edf635fce2c8400d6453967cfde9
Tyler Pflueger <TylerPflueger@hotmail.com>
2016-08-04 23:36:05 -0500

    Fix webpack-loader
    Revert elm-webpack-loader to version that works on linux

--- src/Main.js
+++ src/Main.js
@@ -6,4 +6,4 @@ var Elm = require('./Main.elm');
 var mountNode = document.getElementById('main');
 
 // The third value on embed are the initial values for incomming ports into Elm
-var app = Elm.Main.embed(mountNod e);
+var app = Elm.Main.embed(mountNode);
