773e0dd902fc5c5b8f8cc4fb50fc0f67d7da1fd1
Joe Aulner <joseph.aulner@aviture.us.com>
2016-08-08 23:07:51 -0500

    Replace tabs with spaces in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,68 +6,68 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 require('es6-promise').polyfill();
 
 const PATHS = {
-	app: path.join(__dirname, 'src/main.js'),
-	dest: path.join(__dirname, 'dest')
+    app: path.join(__dirname, 'src/main.js'),
+    dest: path.join(__dirname, 'dest')
 };
 
 module.exports = {
-	devServer: {
-		contentBase: PATHS.dest,
-		historyApiFallback: true,
-		inline: true,
-		progress: true
-	},
-	entry: {
-		app: PATHS.app
-	},
-	output: {
-		path: PATHS.dest,
-		filename: '[name].js'
-	},
-	watchOptions: {
-		poll: 1000,
-		aggregateTimeout: 1000
-	},
-	plugins: [
+    devServer: {
+        contentBase: PATHS.dest,
+        historyApiFallback: true,
+        inline: true,
+        progress: true
+    },
+    entry: {
+        app: PATHS.app
+    },
+    output: {
+        path: PATHS.dest,
+        filename: '[name].js'
+    },
+    watchOptions: {
+        poll: 1000,
+        aggregateTimeout: 1000
+    },
+    plugins: [
         new HtmlWebpackPlugin({
             title: 'snaks'
         }),
-		new CleanWebpackPlugin(['dest'], {
-			root: __dirname,
-			verbose: true,
-			dry: false
-		})
-	],
-	module: {
-		loaders: [
-			{
-				test: /\.(less)$/,
-				loaders: [
-					'style-loader',
-					'css-loader',
+        new CleanWebpackPlugin(['dest'], {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.(less)$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader',
                     'less-loader'
-				]
-			},
-			{
-				test:    /\.html$/,
-				exclude: /node_modules/,
-				loader:  'file?name=[name].[ext]',
-			},
-			{
-				test:    /\.elm$/,
-				exclude: [/elm-stuff/, /node_modules/],
-				loader:  'elm-webpack',
-			},
-			{
-				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: 'url-loader?limit=10000&minetype=application/font-woff',
-			},
-			{
-				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				loader: 'file-loader',
-			},
-		],
+                ]
+            },
+            {
+                test:    /\.html$/,
+                exclude: /node_modules/,
+                loader:  'file?name=[name].[ext]',
+            },
+            {
+                test:    /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loader:  'elm-webpack',
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&minetype=application/font-woff',
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader',
+            },
+        ],
 
-		noParse: /\.elm$/,
-	}
+        noParse: /\.elm$/,
+    }
 };
