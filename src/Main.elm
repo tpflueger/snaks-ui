0ea1cee467c79ca0f8d00e52b2064493f0639f52
Joe Aulner <joseph.aulner@aviture.us.com>
2016-08-11 20:40:13 -0500

    Add basic map / snake rendering logic

--- src/Main.elm
+++ src/Main.elm
@@ -5,8 +5,9 @@ import Color
 import Html exposing (Html, div)
 import Html.Attributes as Attr
 import Html.App
-import Collage as Coll exposing (collage)
+import Collage exposing (Form)
 import Element exposing (toHtml)
+import Transform
 
 
 -- MAIN
@@ -28,12 +29,32 @@ main =
 type alias Model =
     { width : Int
     , height : Int
+    , snake : Snake
     }
+
+
+type alias Snake =
+    List Vector
+
+
+type alias Vector =
+    { x : Int
+    , y : Int
+    }
+
+
+mapSize : Int
+mapSize = 16
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model 0 0, Cmd.none )
+    ( Model mapSize mapSize initSnake, Cmd.none )
+
+
+initSnake : Snake
+initSnake =
+    [ Vector 2 1, Vector 2 0, Vector 1 0, Vector 0 0 ]
 
 
 -- UPDATE
@@ -53,6 +74,10 @@ update msg model =
 -- VIEW
 
 
+tileSize : Int
+tileSize = 16
+
+
 (=>) : String -> String -> (String, String)
 (=>) x y = (x, y)
 
@@ -61,9 +86,53 @@ view : Model -> Html Msg
 view model =
     div [ Attr.style [ "width" => "100%", "height" => "100%" ] ]
         [ toHtml <|
-            collage model.width model.height
-                [ Coll.filled Color.blue <| Coll.rect (toFloat model.width) (toFloat model.height) ]
+            Collage.collage 500 500 <|
+                [ renderMap model
+                , renderObjects model
+                ]
         ]
+
+
+renderObjects : Model -> Form
+renderObjects model =
+    List.map renderSegment model.snake
+        |> translateObjects
+
+
+renderMap : Model -> Form
+renderMap model =
+    let
+        width = model.width * tileSize |> toFloat
+        height = model.height * tileSize |> toFloat
+    in
+        Collage.rect width height
+            |> Collage.filled Color.blue
+
+
+renderSegment : Vector -> Form
+renderSegment vector =
+    let
+        tileSize' = toFloat tileSize
+        offsetX = tileSize * vector.x |> toFloat
+        offsetY = tileSize * vector.y |> toFloat
+        outline = 
+            Collage.rect tileSize' tileSize'
+                |> Collage.filled Color.white
+        body =
+            Collage.rect (tileSize' - 1) (tileSize' - 1)
+                |> Collage.filled Color.red
+    in
+        Collage.group [ outline, body ]
+            |> Collage.move (offsetX, offsetY)
+
+
+translateObjects : List Form -> Form
+translateObjects objects =
+    let
+        offset = toFloat <| -mapSize * tileSize // 2 + tileSize // 2
+        transformation = Transform.translation offset offset
+    in
+        Collage.groupTransform transformation objects
 
 
 -- SUBSCRIPTIONS
